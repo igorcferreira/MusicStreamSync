cb45ef6967a5e299cae07a8de0df5e27cf13f6a8
Igor Ferreira <me@igorcferreira.dev>
2025-04-14 23:58:24 +0200

    Prepare environment

--- shared/src/native/KCrypto/Package.swift
+++ shared/src/native/KCrypto/Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "KCrypto",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .macCatalyst(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "KCrypto", type: .static, targets: ["KCrypto"]),
