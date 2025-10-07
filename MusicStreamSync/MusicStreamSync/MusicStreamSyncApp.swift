7b3944552105e626436a93d238feb28452f9d373
Igor Ferreira <me@igorcferreira.dev>
2025-10-07 14:38:28 +0200

    Structure keys

---
+++ MusicStreamSync/MusicStreamSync/MusicStreamSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicStreamSyncApp.swift
+//  MusicStreamSync
+//
+//  Created by Igor Ferreira on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicStreamSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
