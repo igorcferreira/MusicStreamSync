a9e30a5194cf5458f079ce5a3a338b4a318dbbcc
Igor Ferreira <me@igorcferreira.dev>
2025-10-07 14:38:28 +0200

    Structure keys

---
+++ LastFMClient/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LastFMClient",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LastFMClient",
+            targets: ["LastFMClient"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "24.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LastFMClient",
+            dependencies: [
+                .product(name: "KeychainSwift", package: "keychain-swift")
+            ]
+        ),
+
+    ]
+)
---
+++ MusicStreamSync/MusicStreamSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicStreamSync
+//
+//  Created by Igor Ferreira on 7/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
