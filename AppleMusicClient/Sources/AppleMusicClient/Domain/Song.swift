315b291ede88ca249eaf4837c59c922439382d43
Igor Ferreira <me@igorcferreira.dev>
2025-10-09 17:57:07 +0200

    Basic search

---
+++ AppleMusicClient/Sources/AppleMusicClient/Domain/Song.swift
@@ -0,0 +1,22 @@
+//
+//  Song.swift
+//  AppleMusicClient
+//
+//  Created by Igor Ferreira on 9/10/25.
+//
+import Foundation
+import MusicKit
+
+extension Song {
+    func toPlayerItem() -> PlayingItem {
+        .init(
+            id: id.rawValue,
+            title: title,
+            artist: artistName,
+            duration: duration ?? 0.0,
+            album: albumTitle ?? "",
+            url: url,
+            artwork: artwork?.url
+        )
+    }
+}
--- MusicStreamSync/MusicStreamSync/UI/Home/SearchView.swift
+++ MusicStreamSync/MusicStreamSync/UI/Home/SearchView.swift
@@ -5,24 +5,66 @@
 //  Created by Igor Ferreira on 9/10/25.
 //
 import SwiftUI
+import AppleMusicClient
+import NukeUI
 
 struct SearchView: View {
+    @Environment(\.appleMusicClient) var appleMusicClient
+    @Environment(\.playerBridge) var playerBridge
     @State private var search: String = ""
     @FocusState private var searchFocus: Bool
+    @State private var items: [PlayingItem] = []
     
     var body: some View {
         Grouping {
-            Text("SearchView")
-                .searchable(text: $search)
+            List(items) { item in
+                HStack {
+                    if let artwork = item.artwork {
+                        LazyImage(url: artwork) { phase in
+                            phase.image?.resizable()
+                        }
+                        .frame(width: 40, height: 40)
+                        .cornerRadius(4.0)
+                    }
+                    Text(item.title)
+                }
+                .onTapGesture {
+                    play(item: item)
+                }
+            }
+            .searchable(text: $search)
         }
         .searchFocused($searchFocus)
-        .onAppear {
+        .task {
             searchFocus = true
+        }
+        .onChange(of: search) { _, newValue in
+            search(term: newValue)
         }
     }
     
+    func play(item: PlayingItem) { Task {
+        await playerBridge.play(item)
+    }}
+    
+    private func search(term: String) { Task {
+        let trimmed = term.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !trimmed.isEmpty else {
+            await update(search: [])
+            return
+        }
+        
+        let items = await appleMusicClient.searchSong(term: trimmed)
+        await update(search: items)
+    }}
+    
+    @MainActor
+    private func update(search: [PlayingItem]) async {
+        items = search
+    }
+    
     @ViewBuilder
-    func Grouping<Content: View>(
+    private func Grouping<Content: View>(
         @ViewBuilder body: () -> Content
     ) -> some View {
         #if os(macOS)
