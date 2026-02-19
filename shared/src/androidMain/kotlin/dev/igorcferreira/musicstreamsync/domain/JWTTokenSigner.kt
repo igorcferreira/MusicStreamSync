a1fb0e42c20cb08cda7729198ae6d021799625d9
Igor Ferreira <me@igorcferreira.dev>
2026-02-20 00:03:48 +0100

    Update dependencies

--- shared/src/androidMain/kotlin/dev/igorcferreira/musicstreamsync/domain/JWTTokenSigner.kt
+++ shared/src/androidMain/kotlin/dev/igorcferreira/musicstreamsync/domain/JWTTokenSigner.kt
@@ -2,17 +2,17 @@ package dev.igorcferreira.musicstreamsync.domain
 
 import dev.igorcferreira.musicstreamsync.model.jwt.JWTHeader
 import io.jsonwebtoken.Jwts
-import io.ktor.util.*
 import kotlinx.serialization.json.Json
 import java.security.KeyFactory
 import java.security.spec.PKCS8EncodedKeySpec
+import kotlin.io.encoding.Base64
 
 class JWTTokenSigner : TokenSigner {
     private val json = Json { ignoreUnknownKeys = true }
 
     override suspend fun sign(jwtToken: String, privateKey: String): String {
         val components = jwtToken.split(".")
-        val header = json.decodeFromString<JWTHeader>(components[0].decodeBase64String())
+        val header = json.decodeFromString<JWTHeader>(Base64.decode(components[0]).decodeToString())
 
         val spec = PKCS8EncodedKeySpec(privateKey.clearKey())
         val keyFactory = KeyFactory.getInstance("EC")
@@ -22,7 +22,7 @@ class JWTTokenSigner : TokenSigner {
             .header()
             .keyId(header.kid)
             .and()
-            .content(components[1].decodeBase64String())
+            .content(Base64.decode(components[1]).decodeToString())
 
         return builder
             .signWith(key, Jwts.SIG.ES256)
